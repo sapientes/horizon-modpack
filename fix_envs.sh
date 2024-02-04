2a582904aeeada4ecfb4a957744cec2e6453c26f
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2024-02-04 17:03:37 +0100

    feat: force side = 'both' for some mods

---
+++ fix_envs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+sed -i 's/^side = "client"/side = "both"/' mods/jei.pw.toml
+sed -i 's/^side = "client"/side = "both"/' mods/just-enough-effect-descriptions-jeed.pw.toml
+sed -i 's/^side = "client"/side = "both"/' mods/create-questing.pw.toml
+sed -i 's/^side = "client"/side = "both"/' mods/do-a-barrel-roll.pw.toml
+sed -i 's/^side = "client"/side = "both"/' mods/just-enough-resources-jer.pw.toml
+sed -i 's/^side = "client"/side = "both"/' mods/libipn.pw.toml
+sed -i 's/^side = "client"/side = "both"/' mods/ponder.pw.toml
+
