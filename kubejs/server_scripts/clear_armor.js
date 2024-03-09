c7fff176b5d2e8b46aff493bbd2df5909251ee21
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2024-03-09 18:58:32 +0100

    fix: remove goofy aah armor

--- kubejs/server_scripts/clear_armor.js
+++
@@ -1,5 +0,0 @@
-PlayerEvents.inventoryChanged((e) => {
-    if (!e.getItem().hasTag("nyxmc:cosmetic_armor")) return;
-
-    e.getEntity().runCommandSilent("execute as @s[tag=!cosmetic_armor] run clear @s #nyxmc:cosmetic_armor");
-});
