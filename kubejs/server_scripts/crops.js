1ff190581cd79b58be3154ff05d5a8c36231f409
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2024-04-29 20:11:31 +0200

    feat: remove ars noveau

--- kubejs/server_scripts/crops.js
+++ kubejs/server_scripts/crops.js
@@ -6,14 +6,6 @@ ServerEvents.recipes(event => {
             'gardens_of_the_dead:whistlecane'
         ]
     )
-    event.recipes.ars_nouveau.crush(
-        'gardens_of_the_dead:whistlecane', // input block
-        [
-            Item.of('create:cinder_flour', 2).withChance(1.0)
-            //{ item: Item.of('minecraft:sand').withChance(1.0), maxRolls: 4 }
-        ], // loot table
-        true // true // drop the item in world?
-    )
     event.recipes.create.crushing(['create:cinder_flour', 1, Item.of('create:cinder_flour', 1).withChance(0.75)], 'gardens_of_the_dead:whistlecane')
     event.recipes.create.milling(['create:cinder_flour', 1, Item.of('create:cinder_flour', 1).withChance(0.75)], 'gardens_of_the_dead:whistlecane')
     event.shaped(
